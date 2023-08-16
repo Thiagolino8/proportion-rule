9e07d8bbe3e4401e7679e30dd811497b2c180713
Thiago <thiagolinog8@gmail.com>
2023-08-16 13:54:11 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Proportion Rule
+
+## Description
+
+This is a simple rule that can be used to calculate the proportion of a given value in a set of values.
+
+## App
+
+The app is available at [https://proportion-rule.herokuapp.com/](https://proportion-rule.herokuapp.com/).
---
+++ src/lib/components/Regra.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import {Input} from './ui/input'
+	import autoAnimate from '@formkit/auto-animate'
+
+	let value1: number
+	let value2: number
+	let value3: number
+
+	$: result = Number(((value3 * value2) / value1).toFixed(2))
+</script>
+
+<main use:autoAnimate class="h-screen grid place-content-center gap-4 font-mono">
+	<h1 class="text-4xl text-center font-extrabold">Proportion Rule</h1>
+	<section class="grid grid-cols-2 gap-4">
+		<Input type="number" bind:value={value1} />
+		<Input type="number" bind:value={value2} />
+		<Input type="number" bind:value={value3} />
+		<Input type="number" bind:value={result} disabled />
+	</section>
+
+	{#if result}
+		{#key result}
+			<p class="text-center text-2xl font-bold font-mono">
+				{value3} is for {result} as {value1} is for {value2}
+			</p>
+		{/key}
+	{/if}
+</main>
